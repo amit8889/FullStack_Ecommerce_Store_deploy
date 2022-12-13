18f2a5bb55c31f1ea0b5ad165b6cfa9cb8591f28
amit5511 <96247681+amit5511@users.noreply.github.com>
2022-12-13 17:53:19 +0530

    Update database.js

--- Backend/config/database.js
+++ Backend/config/database.js
@@ -4,11 +4,15 @@ const mongoose = require('mongoose');
 
 
 const connect=async(URL)=>{
-
-    await mongoose.connect(URL,{
+try{
+  await mongoose.connect(URL,{
         useNewUrlParser:true,useUnifiedTopology:true
     });
     console.log("Mongodb connected with server : localhost");
+}catch(error){
+ console.log("Not Connected tO Database : "+error.message);
+}   
+  
 
 //in server js unhandled rehjection all radey handled so no need to use try catch here  
 }
